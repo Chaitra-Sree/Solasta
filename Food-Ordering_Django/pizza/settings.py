1681d33720980c37b6608c035e6d29cfdc7dd059
Chaitra <163324776+Chaitra-Sree@users.noreply.github.com>
2025-03-29 16:22:28 +0530

    removed qr line in middleware

--- Food-Ordering_Django/pizza/settings.py
+++ Food-Ordering_Django/pizza/settings.py
@@ -68,15 +68,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tinymce',
-    
-    
-    
+   
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'pizza.middleware.QRAccessMiddleware',  
+    'django.contrib.sessions.middleware.SessionMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
